1cc2c3e386f23686deb99218255ae2a75bcee5d3
gardunocesar17-ctrl <gardunocesar17@gmail.com>
2026-07-21 12:40:21 -0600

    espero que ahora si

--- sneakersmor/testHtmlCors.js
+++
@@ -1,5 +0,0 @@
-async function test() {
-  const res = await fetch(`https://airfire.com.mx/products/air-force-1-negro-total`);
-  console.log("HTML GET Headers:", res.headers.get('access-control-allow-origin'));
-}
-test();
